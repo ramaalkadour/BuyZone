1cfe887a5f1750ed17826685000e3798f7a696b8
Rama Alkadour <ramaalkadour271@gmail.com>
2025-07-09 19:02:24 +0300

    ``` feat(Product): Add Get all products query handler
    ```

---
+++ BuyZone/BuyZone.Application/Product/Queries/GetAll/GetAllProductsHandler.cs
@@ -0,0 +1,28 @@
+using BuyZone.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BuyZone.Application.Product.Queries.GetAll;
+
+public class GetAllProductsHandler : IRequestHandler<GetAllProductsQuery.Request, GetAllProductsQuery.Response>
+{
+    private readonly IRepository _repository;
+
+    public GetAllProductsHandler(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<GetAllProductsQuery.Response> Handle(GetAllProductsQuery.Request request, CancellationToken cancellationToken)
+    {
+        var products = await _repository.Query<Domain.Entities.Product>()
+            .Select(GetAllProductsQuery.Response.ProductRes.Selector())
+            .ToListAsync(cancellationToken);
+
+        return new GetAllProductsQuery.Response
+        {
+            Count = products.Count,
+            Products = products
+        };
+    }
+}
---
+++ BuyZone/BuyZone.Application/Product/Queries/GetAll/GetAllProductsQuery.cs
@@ -0,0 +1,37 @@
+using System.Linq.Expressions;
+using MediatR;
+
+namespace BuyZone.Application.Product.Queries.GetAll;
+
+public class GetAllProductsQuery
+{
+    public class Request : IRequest<Response>
+    {
+    }
+
+    public class Response
+    {
+        public int Count { get; set; }
+        public List<ProductRes> Products { get; set; } = new();
+
+        public class ProductRes
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+            public string ImageUrl { get; set; }
+            public string Description { get; set; }
+            public double Price { get; set; }
+            public Guid CategoryId { get; set; }
+
+            public static Expression<Func<Domain.Entities.Product, ProductRes>> Selector() => e => new ProductRes
+            {
+                Id = e.Id,
+                Name = e.Name,
+                ImageUrl = e.ImageUrl,
+                Description = e.Description,
+                Price = e.Price,
+                CategoryId = e.CategoryId
+            };
+        }
+    }
+}
