1b4214e465d76ff85962a63e34bf3d1a18c96bc7
Rama Alkadour <ramaalkadour271@gmail.com>
2025-07-09 16:29:23 +0300

    ``` 🐛 fix(Customer Query): Implement GetByIdCustomerHandler
    Adds the GetByIdCustomerHandler to retrieve customer data by ID using the repository.
    ```

--- BuyZone/BuyZone.Application/Customer/Queries/GetById/GetByIdCustomerHandler.cs
+++ BuyZone/BuyZone.Application/Customer/Queries/GetById/GetByIdCustomerHandler.cs
@@ -1,6 +1,31 @@
+using BuyZone.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
 namespace BuyZone.Application.Customer.Queries.GetById;
 
-public class GetByIdCustomerHandler
+public class GetByIdCustomerHandler:IRequestHandler<GetByIdCustomerQuery.Request, GetByIdCustomerQuery.Response>
 {
-    
+    private readonly IRepository _repository;
+
+    public GetByIdCustomerHandler(IRepository repository)
+    {
+        _repository = repository;
+    }
+    public GetByIdCustomerQuery.Response Handle(GetByIdCustomerQuery.Request request, CancellationToken cancellationToken)
+    {
+        var customer = _repository.Query<Domain.Entities.Security.Customer>()
+            .Where(c => c.Id == request.Id)
+            .Select(GetByIdCustomerQuery.Response.Selector())
+            .FirstOrDefault(cancellationToken);
+
+        if (customer == null)
+            return null;
+
+        return new GetByIdCustomerQuery.Response
+        {
+            Customer = customer
+        };
+    }
+}
 }
