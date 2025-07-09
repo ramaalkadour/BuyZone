a30f5515e1eca5b89580ac15fca77eef6f85caa2
Rama Alkadour <ramaalkadour271@gmail.com>
2025-07-10 00:10:33 +0300

    ✅ refactor(Order): Implement AddOrderCommand and AddOrderHandler

--- BuyZone/BuyZone.Application/Order/Commands/Add/AddOrderCommand.cs
+++ BuyZone/BuyZone.Application/Order/Commands/Add/AddOrderCommand.cs
@@ -1,6 +1,17 @@
+using BuyZone.Application.Order.Queries.GetAll;
+using BuyZone.Application.Order.Queries.GetById;
+using MediatR;
+
 namespace BuyZone.Application.Order.Commands.Add;
 
 public class AddOrderCommand
 {
-    
+    public class Request: IRequest<GetAllOrdersQuery.Response.OrdersRes>
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+    }
 }
--- BuyZone/BuyZone.Application/Order/Commands/Add/AddOrderHandler.cs
+++ BuyZone/BuyZone.Application/Order/Commands/Add/AddOrderHandler.cs
@@ -1,6 +1,20 @@
+using BuyZone.Application.Order.Queries.GetAll;
+using BuyZone.Domain.BaseUser;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
 namespace BuyZone.Application.Order.Commands.Add;
 
-public class AddOrderHandler
+public class AddOrderHandler:IRequestHandler<AddOrderCommand.Request, GetAllOrdersQuery.Response.OrdersRes>
 {
-    
+    private readonly UserManager<User> _userManager;
+
+    public AddOrderHandler(UserManager<User> userManager)
+    {
+        _userManager = userManager;
+    }
+    public Task<GetAllOrdersQuery.Response.OrdersRes> Handle(AddOrderCommand.Request request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
